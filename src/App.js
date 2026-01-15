575e48dc0b942f6ac589c0ea96a11ac273f9f93a
saikripakks <95914702+saikripakks@users.noreply.github.com>
2026-01-16 01:38:07 +0530

    change docker file on vx

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Jenkins on Udemy Hi Guys...
+          Learn Jenkins on Kripa
         </a>
       </header>
       <p>
